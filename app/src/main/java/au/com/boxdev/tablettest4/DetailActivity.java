a3e307e1701d5f64606fad85c8c4789f8ecc3e76
Ian Murnane <imurnane@internode.on.net>
2017-07-10 19:22:40 +1000

    Code cleaned up and comments added

--- app/src/main/java/au/com/boxdev/tablettest4/DetailActivity.java
+++ app/src/main/java/au/com/boxdev/tablettest4/DetailActivity.java
@@ -1,11 +1,7 @@
 package au.com.boxdev.tablettest4;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class DetailActivity extends AppCompatActivity {
 
